b83827d86d77a52970772c6b52d996b4bd20cb23
Toby Dawson <tobydawson1@gmail.com>
2020-08-20 14:20:45 +0100

    test drived upper limits of grid

---
+++ lib/mars_plateau.rb
@@ -0,0 +1,10 @@
+class Mars
+
+    attr_reader :upper_x_axis, :upper_y_axis
+
+    def initialize(x, y)
+        @upper_x_axis =  x
+        @upper_y_axis = y
+    end
+
+end
---
+++ spec/unit/mars_spec.rb
@@ -0,0 +1,17 @@
+require 'mars_plateau'
+
+describe Mars do
+
+    before :each do
+        @mars = Mars.new(3,3)
+    end
+
+    it 'can be initiazed' do
+        expect(@mars).to be_an_instance_of(Mars)
+    end
+
+    it 'initializes with the size of the playeau' do
+        @mars = Mars.new(3, 3)
+        expect(@mars.upper_x_axis).to equal(3)
+    end
+end
