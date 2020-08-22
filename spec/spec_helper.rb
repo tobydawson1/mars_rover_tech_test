5966c35973276a32943859e20e17c3d6ed0c12aa
Toby Dawson <tobydawson1@gmail.com>
2020-08-22 11:23:05 +0100

    refactored

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
-require 'simplecov'
-require 'simplecov-console'
 require 'coveralls'
 Coveralls.wear!
+require 'simplecov'
+require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
