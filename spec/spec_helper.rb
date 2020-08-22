ace2f89be670ca3f111f493b935ed7bd1174c6cc
Toby Dawson <tobydawson1@gmail.com>
2020-08-22 11:32:36 +0100

    refactored

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,9 @@ Coveralls.wear!
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
 ])
 SimpleCov.start
 
