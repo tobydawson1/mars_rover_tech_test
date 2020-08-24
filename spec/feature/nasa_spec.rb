a228b828bd8c2973b851a8cdc5bb8f296149fd29
Toby Dawson <tobydawson1@gmail.com>
2020-08-24 10:40:15 +0100

    added irb example screenshot

--- spec/feature/nasa_spec.rb
+++ spec/feature/nasa_spec.rb
@@ -6,9 +6,22 @@ describe Nasa do
     @nasa = Nasa.new("5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM")
   end
 
-  it 'moves rover one' do
+  it 'gives the correct final positions of both rovers' do
     expect(@nasa.mission_one).to eq("1 3 N")
     expect(@nasa.mission_two).to eq("5 1 E")
   end
   
+end
+
+describe Nasa do 
+
+  before :each do
+    @nasa = Nasa.new("10 10", "5 2 W", "LMMLMLMMM", "4 4 S", "MMMLMMLMMMMM")
+  end
+
+  it 'gives another set of correct rover positions' do
+    expect(@nasa.mission_one).to eq("6 1 S")
+    expect(@nasa.mission_two).to eq("2 6 N")
+  end
+
 end
